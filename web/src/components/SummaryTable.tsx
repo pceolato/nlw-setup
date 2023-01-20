2abd8f038e170459b7dcc3c7f44b796539da1f85
= <pedroceolato2002@gmail.com>
2023-01-20 10:18:16 -0300

    feat: add modal, popover and progressbar of habit

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -27,7 +27,11 @@ export function SummaryTable() {
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {
                     summaryDates.map(date => (
-                        <HabitDay key={date.toString()}/>
+                        <HabitDay 
+                            key={date.toISOString()}
+                            amount={5}
+                            completed={Math.round(Math.random() * 5)}
+                        />
                     ))
                 }
                 {
